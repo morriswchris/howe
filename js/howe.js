2956975802732ad92676e53291c26669bbb74c1c
Chris Morris <morris.w.chris@gmail.com>
2016-03-25 00:20:16 -0400

    updated hash changes

--- js/howe.js
+++ js/howe.js
@@ -1,53 +1,63 @@
 //main
 $(document).ready(function() {
-    var services = {
-        "plex": "32400/manage/index.html#!/dashboard",
-        "couchpotato": "5050/home/",
-        "sickbeard": "8081/home/",
-        "transmission": "9091/transmission/web/",
-        "sabnzbd": "8082/sabnzbd/"
-    };
+  var services = {
+    "plex": "32400/manage/index.html#!/dashboard",
+    "couchpotato": "5050/home/",
+    "sickbeard": "8081/home/",
+    "sabnzbd": "8082/sabnzbd/"
+  };
 
-    function service_switch(e) {
 
-        e.preventDefault();
-        var service = $(this).data("id"),
-            domain = 'http://' + document.domain + ":",
-            serviceUrl = services[service];
-        if (serviceUrl) {
-            $("iframe").attr('src', domain+serviceUrl);
-            $('.sidebar').sidebar("hide");
-            $(".item").removeClass("active");
-            $(this).addClass("active");
-        }
-        return false;
+
+  function service_switch(e) {
+    e.preventDefault();
+    var service = $(this).data("id");
+    frame_switch(service)
+    return false;
+  }
+
+  function frame_switch(service) {
+    var serviceUrl = services[service],
+    domain = 'http://' + document.domain + ":";
+    if (serviceUrl) {
+      window.location.href = "#"+service;
+      $("iframe").attr('src', domain+serviceUrl);
+      $('.sidebar').sidebar("hide");
+      $(".item").removeClass("active");
+      $(this).addClass("active");
     }
+  }
 
-    function frameResize() {
-        var frame = $("#service-frame");
-        if (frame.length > 0) {
-            var documentHeight = $(window).innerHeight(),
-                newHeight = (documentHeight) + 'px';
-            frame.css('height', newHeight);
-        }
+  function frameResize() {
+    var frame = $("#service-frame");
+    if (frame.length > 0) {
+      var documentHeight = $(window).innerHeight(),
+      newHeight = (documentHeight) + 'px';
+      frame.css('height', newHeight);
     }
+  }
 
-    $(".sidebar .item").on("click", service_switch);
-    $('.sidebar').sidebar('attach events', '#menu-btn');
+  $(".sidebar .item").on("click", service_switch);
+  $('.sidebar').sidebar('attach events', '#menu-btn');
 
-    $(window).resize(function() {
-        frameResize();
-    });
-
-    $("#close").on("click", function(){
-        $('.sidebar').sidebar('hide');
-    });
-
-    //main
+  $(window).resize(function() {
     frameResize();
-    var items = $(".item .menu .item");
-    if( items[0]){
-        $(items[0]).trigger("click");
-    }
-    $('.sidebar').sidebar('show');
+  });
+
+  $("#close").on("click", function(){
+    $('.sidebar').sidebar('hide');
+  });
+
+  //main
+  frameResize();
+  var items = $(".item .menu .item"),
+  hash = window.location.hash.replace("#", "");
+
+  if(services[hash]) {
+    frame_switch(hash);
+  }
+  else if( items[0]){
+    $(items[0]).trigger("click");
+  }
+  $('.sidebar').sidebar('show');
 });
